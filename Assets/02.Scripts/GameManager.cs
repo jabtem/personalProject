5bc88fc5fa55aa87bc8e2a319343188ee24ae00f
jabtem <suchie777@gmail.com>
2021-10-18 02:38:49 +0900

    로딩씬추가중 모든 로딩이 필요한과정은 로딩씬을 거치고 그다음 씬으로 넘어가게 구성할 계획

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = null;
+
+
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("GameManager is NULL");
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadScene(int num)
+    {
+        SceneManager.LoadScene(num);
+    }
+}
---
+++ Assets/LoadingManger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingManger : MonoBehaviour
+{
+    public Image loadingBar;
+
+    void Awake()
+    {
+        loadingBar.fillAmount = 0;
+    }
+
+    private void Update()
+    {
+
+        loadingBar.fillAmount += Time.deltaTime;
+    }
+}
