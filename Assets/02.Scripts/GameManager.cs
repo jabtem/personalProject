e13ab43690b025688b87bfc0388fd44b63b33ba9
jabtem <suchie777@gmail.com>
2021-10-18 16:05:53 +0900

    로딩화면 완성 씬으로 별도 구성안하고 UI를 활용해 로딩화면 구현

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+enum Child {LoadinBar}
+
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance = null;
-
+    GameManager[] objs;
+    GameObject loading;
+    LoadingManger loadingManger;
 
     public static GameManager instance
     {
@@ -22,10 +26,27 @@ public class GameManager : MonoBehaviour
     {
         _instance = this;
         DontDestroyOnLoad(this.gameObject);
+        objs = FindObjectsOfType<GameManager>();
+        loading = transform.GetChild((int)Child.LoadinBar).gameObject;
+        loadingManger = gameObject.GetComponent<LoadingManger>();
+        //게임매니저는 항상 한개만 존재해야한다
+        //여러개 존재하면 하나빼고 다삭제
+        if (objs.Length != 1)
+        {
+            for (int i = 1; i < objs.Length; i++)
+            {
+                Destroy(objs[i].gameObject);
+            }
+        }
     }
 
     public void LoadScene(int num)
     {
-        SceneManager.LoadScene(num);
+        SceneManager.LoadSceneAsync(num);
+    }
+    public void LoadLoadingScene(int num)
+    {
+        //로딩씬을불러옴
+        loadingManger.LoadingScene(num);
     }
 }
---
+++ Assets/02.Scripts/LoadingManger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LoadingManger : MonoBehaviour
+{
+    public Image loadingBar;
+    public CanvasGroup loadingBarCanvas;
+
+    void Awake()
+    {
+        loadingBar.fillAmount = 0;
+    }
+
+    public void LoadingScene(int num)
+    {
+        loadingBarCanvas.gameObject.SetActive(true);
+        SceneManager.sceneLoaded += SceneLoadedEnd;
+        StartCoroutine(Load(num));
+    }
+
+    private IEnumerator Load(int sceneIndex)
+    {
+        loadingBar.fillAmount = 0;
+        float timer =0;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
+        while(!operation.isDone)
+        {
+            
+            yield return null;
+            timer += Time.unscaledDeltaTime;
+
+            if (operation.progress<0.9f)
+            {
+                Debug.LogError(operation.progress);
+                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, operation.progress, timer);
+                if(loadingBar.fillAmount >= operation.progress)
+                {
+                    timer = 0f;
+                }
+
+            }
+            else
+            { 
+                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, 1f, timer);
+
+                if(loadingBar.fillAmount == 1.0f)
+                {
+                    operation.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+
+        }
+
+    }
+
+    private void SceneLoadedEnd(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        loadingBarCanvas.gameObject.SetActive(false);
+        SceneManager.sceneLoaded -= SceneLoadedEnd;
+
+    }
+}
