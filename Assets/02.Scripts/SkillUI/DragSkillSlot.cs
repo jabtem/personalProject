4e20b21bfa776e720ac01cabd504b780b3dd851a
jabtem <suchie777@gmail.com>
2021-11-17 21:17:30 +0900

    스킬슬롯 추가중 스킬창에서 드래그드롭으로 조작버튼과 교체할수있게 만들예정

---
+++ Assets/02.Scripts/SkillUI/DragSkillSlot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragSkillSlot : MonoBehaviour
+{
+
+    //스킬 드래그&드롭용슬롯이 단하나만 존재하기때문에 허용
+    static public DragSkillSlot instance;
+    Image skillImage;
+
+
+    //this 안붙이면 스택오버플로우
+    public SkillSlot dragSlot
+    {
+        get
+        {
+            return _dragSlot;
+        }
+        set
+        {
+            _dragSlot = value;
+        }
+    }
+    [SerializeField]
+    SkillSlot _dragSlot;
+
+
+    void Awake()
+    {
+        instance = this;
+        skillImage = GetComponent<Image>();
+    }
+
+    public void DragSetImage(Image _skillImage)
+    {
+        skillImage.sprite = _skillImage.sprite;
+    }
+    public void SetColor(float _alpha)
+    {
+        Color color = skillImage.color;
+        color.a = _alpha;
+        skillImage.color = color;
+    }
+}
---
+++ Assets/02.Scripts/SkillUI/SkillSlot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SkillSlot : MonoBehaviour ,IBeginDragHandler, IDragHandler , IEndDragHandler,IDropHandler
+{
+    public Image skillImage;
+
+    //스킬사용버튼과 스킬슬롯 구분용
+    public bool canDrop;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        DragSkillSlot.instance.dragSlot = this;
+        DragSkillSlot.instance.DragSetImage(skillImage);
+        DragSkillSlot.instance.SetColor(1);
+        DragSkillSlot.instance.transform.position = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (DragSkillSlot.instance.dragSlot !=null)
+        {
+            DragSkillSlot.instance.transform.position = eventData.position;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("drop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        DragSkillSlot.instance.SetColor(0);
+        Debug.Log("end");
+    }
+}
