ecf3e5c5a71601850fe53f73bf772ca5b54b74f3
jabtem <suchie777@gmail.com>
2021-12-03 03:38:34 +0900

    스킬 쿨타임 기능 추가중 쿨타임 추가후 스킬 사용후 대기로 돌아가는 로직 수정필요

---
+++ Assets/02.Scripts/SkillUI/SkillCoolTime.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SkillCoolTime : MonoBehaviour
+{
+    public Image coolTimeImage;
+}
