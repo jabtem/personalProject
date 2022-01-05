4768eb150ffb57c97490de032d828852447aee2e
jabtem <suchie777@gmail.com>
2022-01-05 19:01:20 +0900

    시야각 디버그레이와 기즈모 출력 으로 에디터확인할수잇도록 구현 콜라이더없이 부채꼴 충돌로직을 이용해 부채꼴충돌 구현예정

--- Assets/02.Scripts/Monster/MonsterFOVTest.cs
+++ Assets/02.Scripts/Monster/MonsterFOVTest.cs
@@ -1,18 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class MonsterFOVTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Start is called before the first frame updat;
+
+
+    [Range(0f,180f)]
+    public float angle = 45f;
+    public float radius = 5f;
+    Vector3 rightVector;
+    Vector3 leftVector;
+
+    bool isCol = false;
+    bool gameStart = false;
+
+
+    private void Awake()
     {
-        
+        gameStart = true;
+    }
+    private void Update()
+    {
+
+        rightVector = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+        leftVector = new Vector3(-Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+
+        Debug.DrawRay(transform.position, transform.forward * radius, Color.white);
+        Debug.DrawRay(transform.position, transform.TransformDirection(rightVector.normalized) * radius, Color.red);
+        Debug.DrawRay(transform.position, transform.TransformDirection(leftVector.normalized) * radius, Color.green);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
-        
+        if(gameStart)
+        {
+            Handles.color = isCol ? Color.red : Color.blue;
+            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, angle, radius);
+            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -angle, radius);
+        }
+
     }
 }
