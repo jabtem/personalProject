9ca40dc3cef9fe6b65a3dd45b46f6e461f988e39
jabtem <suchie777@gmail.com>
2022-01-05 22:45:00 +0900

    부채꼴과 점사이의 충돌구현 완료 부채꼴과 원사이의 충돌을 구현해 캐릭터 컨트롤러의 콜라이더 반지름을 이용해서 좀더 정확한 충돌감지를 할수있도록 수정예정

--- Assets/02.Scripts/Monster/MonsterFOVTest.cs
+++ Assets/02.Scripts/Monster/MonsterFOVTest.cs
@@ -8,25 +8,52 @@ public class MonsterFOVTest : MonoBehaviour
     // Start is called before the first frame updat;
 
 
-    [Range(0f,180f)]
+    [Range(0f,360f)]
     public float angle = 45f;
+    [Range(0f,20f)]
     public float radius = 5f;
     Vector3 rightVector;
     Vector3 leftVector;
+    public Transform target;
+    public float playerColRadius;
+    Vector3 targetDirection;
 
     bool isCol = false;
     bool gameStart = false;
 
 
+
     private void Awake()
     {
         gameStart = true;
+        //현재 플레이어가 캐릭터 컨트롤러를 사용하므로
+        playerColRadius = target.GetComponent<CharacterController>().radius;
     }
     private void Update()
     {
 
-        rightVector = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
-        leftVector = new Vector3(-Mathf.Sin(angle * Mathf.Deg2Rad), 0, Mathf.Cos(angle * Mathf.Deg2Rad));
+        rightVector = new Vector3(Mathf.Sin(angle*0.5f * Mathf.Deg2Rad), 0, Mathf.Cos(angle*0.5f * Mathf.Deg2Rad));
+        leftVector = new Vector3(-Mathf.Sin(angle*0.5f * Mathf.Deg2Rad), 0, Mathf.Cos(angle*0.5f * Mathf.Deg2Rad));
+
+        
+
+        //대상의 높이를 고려하지않음 자기자신의높이를기준
+        targetDirection = new Vector3(target.position.x,transform.position.y,target.position.z) - transform.position;
+
+        // 백터의 내적 = 벡터A크기 * 벡터B크기 * Cos(theta)
+        // Cos(theta) = 백터의내적 / 벡터A크기 / 벡터B크기
+        // theta = ACos(벡터의 내적/ 벡터A크기/ 벡터 B크기)
+        //플레이어 정면을 축으로 대상과의 각도가 angle의 절반보다 작거나 같다 => 시야범위내에 있다.
+        //대상과의 거리가 부채꼴 반지름보다 작다 => 시야범위 내에 있다.
+        if (targetDirection.magnitude < radius && Mathf.Acos(Vector3.Dot(transform.forward, targetDirection.normalized)) * Mathf.Rad2Deg <= angle * 0.5f)
+        {
+            isCol = true;
+        }
+        else
+            isCol = false;
+
+
+        
 
         Debug.DrawRay(transform.position, transform.forward * radius, Color.white);
         Debug.DrawRay(transform.position, transform.TransformDirection(rightVector.normalized) * radius, Color.red);
@@ -38,9 +65,10 @@ public class MonsterFOVTest : MonoBehaviour
     {
         if(gameStart)
         {
-            Handles.color = isCol ? Color.red : Color.blue;
-            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, angle, radius);
-            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -angle, radius);
+            Handles.color = isCol ? new Color(1f, 0, 0, 0.2f) : new Color(0, 0, 1f, 0.2f);
+            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, angle*0.5f, radius);
+            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -angle*0.5f, radius);
+            Handles.Label(transform.position+transform.forward *2f, angle.ToString());
         }
 
     }
