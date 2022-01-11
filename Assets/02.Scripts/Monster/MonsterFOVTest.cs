9933e142c474cfa16f1ce4d249848eb13c772d76
jabtem <suchie777@gmail.com>
2022-01-11 12:16:42 +0900

    시야각 각도 180도이상되면 충돌체크 오류나는문제 해결

--- Assets/02.Scripts/Monster/MonsterFOVTest.cs
+++ Assets/02.Scripts/Monster/MonsterFOVTest.cs
@@ -32,24 +32,17 @@ public class MonsterFOVTest : MonoBehaviour
     private void Update()
     {
 
-        //몬스터가 바라보고있는방향 = transform.eulerAngles.y
-        rightVector = new Vector3(Mathf.Sin((transform.eulerAngles.y+angle*0.5f) * Mathf.Deg2Rad), 0, Mathf.Cos((transform.eulerAngles.y + angle * 0.5f) * Mathf.Deg2Rad))*radius;
-        leftVector = new Vector3(Mathf.Sin((transform.eulerAngles.y-angle * 0.5f) * Mathf.Deg2Rad), 0, Mathf.Cos((transform.eulerAngles.y - angle * 0.5f) * Mathf.Deg2Rad))*radius;
+        //시야각 오른쪽 경계선 벡터
+        rightVector = new Vector3(Mathf.Sin((transform.eulerAngles.y+angle*0.5f) * Mathf.Deg2Rad), 
+            0, Mathf.Cos((transform.eulerAngles.y + angle * 0.5f) * Mathf.Deg2Rad))*radius;
+        //시야각 왼쪽 경계선 벡터
+        leftVector = new Vector3(Mathf.Sin((transform.eulerAngles.y-angle * 0.5f) * Mathf.Deg2Rad), 
+            0, Mathf.Cos((transform.eulerAngles.y - angle * 0.5f) * Mathf.Deg2Rad))*radius;
+        //오브젝트에서 타겟방향 벡터
         targetDirection = new Vector3(target.position.x,transform.position.y,target.position.z) - transform.position;
 
-        //Debug.Log("Target to rightVec distance :" + Vector3.Cross(rightVector, targetDirection).magnitude / radius);
-        //Debug.Log("Target to leftVec distance :" + Vector3.Cross(leftVector, targetDirection).magnitude / radius);
-        //Debug.Log("Target to -rightVec distance :" + Vector3.Cross(-rightVector, targetDirection).magnitude / radius);
-        //Debug.Log("Target to -leftVec distance :" + Vector3.Cross(-leftVector, targetDirection).magnitude / radius);
-        //Debug.Log(Vector3.Cross(leftVector, targetDirection).magnitude);
-        //Debug.Log(targetDirection.magnitude * leftVector.magnitude * Mathf.Sin(Mathf.Acos(Vector3.Dot(targetDirection.normalized, leftVector.normalized))));
-        //Debug.Log("11: " + Vector3.Dot(targetDirection.normalized, leftVector.normalized));
-        //Debug.Log("22: " + Vector3.Dot(targetDirection.normalized, rightVector.normalized));
-
-
-
         /***************************** 부채꼴 원충돌 ********************************/
-        // 타겟과의 거리가 반지름+플레이어의 콜라이더 반지름 보다 작거나같으면
+        // 타겟과의 거리가 (부채꼴반지름 + 플레이어의반지름) 보다 작거나같으면
         // 3가지 경우를 체크
         // 1.오브젝트 정면벡터와 타겟 방향 벡터 사이의 각이 지정한 Angle/2 보다 작거나 같은경우(정면기준 좌우로나뉘기때문에 Angle의 절반씩 확인)
         // -> 시야각 호부분과 시야각 내부와 플레이어가 접촉
@@ -58,9 +51,7 @@ public class MonsterFOVTest : MonoBehaviour
         // 3.부채꼴 오른쪽선과 타겟사이의 거리가 플레이어 콜라이더 반지름보다 작거나 같은경우
         // -> 시야각 오른쪽 경계선에 플레이어가 접촉
         /****************************************************************************/
-        if (targetDirection.sqrMagnitude <= (radius + playerColRadius)* (radius + playerColRadius) &&
-            ((Vector3.Dot(targetDirection.normalized, leftVector.normalized) >= 0) || 
-            (Vector3.Dot(targetDirection.normalized, rightVector.normalized) >= 0)))
+        if (targetDirection.sqrMagnitude <= (radius + playerColRadius)* (radius + playerColRadius))
         {
             //부체꼴 호부분과 원 충돌 체크
             if (Mathf.Acos(Vector3.Dot(transform.forward, targetDirection.normalized)) * Mathf.Rad2Deg <= angle * 0.5f)
@@ -69,28 +60,23 @@ public class MonsterFOVTest : MonoBehaviour
                 isCol = true;
             }
             //부채꼴 왼쪽선과 원 충돌체크
-            else if (Vector3.Cross(leftVector, targetDirection).magnitude / radius <= playerColRadius)
+            //대상이 오브젝트 정면기준으로 왼쪽에 있을때만체크
+            else if (Vector3.Cross(leftVector, targetDirection).magnitude / radius <= playerColRadius &&
+                Vector3.Cross(targetDirection, transform.forward).y >= 0 &&
+                Vector3.Dot(targetDirection.normalized, leftVector.normalized) >= 0)
             {
-                //대상이 오브젝트 정면기준으로 왼쪽에 있을때만체크
-                if (Vector3.Cross(targetDirection, transform.forward).y >= 0)
-                {
-                    Debug.Log(2);
-                    isCol = true;
-                }
-                else
-                    isCol = false;
+
+                Debug.Log(2);
+                isCol = true;
             }
             //부채꼴 오른쪽선과 원 충돌체크
-            else if (Vector3.Cross(rightVector, targetDirection).magnitude / radius <= playerColRadius)
+            //대상이 오브젝트 정면기준으로 오른쪽에 있을때만체크
+            else if (Vector3.Cross(rightVector, targetDirection).magnitude / radius <= playerColRadius &&
+                Vector3.Cross(targetDirection, transform.forward).y < 0&&
+                Vector3.Dot(targetDirection.normalized, rightVector.normalized) >= 0)
             {
-                //대상이 오브젝트 정면기준으로 오른쪽에 있을때만체크
-                if (Vector3.Cross(targetDirection, transform.forward).y < 0)
-                {
-                    Debug.Log(3);
-                    isCol = true;
-                }
-                else
-                    isCol = false;
+                Debug.Log(3);
+                isCol = true;
             }
             else
                 isCol = false;
@@ -105,8 +91,6 @@ public class MonsterFOVTest : MonoBehaviour
         Debug.DrawRay(transform.position, targetDirection, Color.yellow);
         Debug.DrawRay(transform.position, rightVector, Color.red);
         Debug.DrawRay(transform.position, leftVector, Color.green);
-        Debug.DrawRay(transform.position, -rightVector, Color.red);
-        Debug.DrawRay(transform.position, -leftVector , Color.green);
 
 
 
