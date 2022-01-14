922d7fc8442c2d1c5fa60873504c24d1cde01f85
jabtem <suchie777@gmail.com>
2022-01-14 21:18:33 +0900

    커스텀 인스펙터 마무리 , 몹 ai 이동로직 구현중

--- Assets/02.Scripts/Monster/Editor/MnsterMoveInspector.cs
+++ Assets/02.Scripts/Monster/Editor/MnsterMoveInspector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 [CanEditMultipleObjects]//다중선택을 가능하게함
 [CustomEditor(typeof(MonsterMove))]
 public class MnsterMoveInspector : Editor
@@ -22,17 +23,15 @@ public class MnsterMoveInspector : Editor
 
     public override void OnInspectorGUI()
     {
-
-
         //정찰모드가 변경시 값초기화
         EditorGUI.BeginChangeCheck();
         var roamingMode = (MonsterMove.RoamingMode)EditorGUILayout.EnumPopup("정찰 모드", monsterMove.roamingMode);
         if(EditorGUI.EndChangeCheck())
         {
-
+            RoamingModeChangeTest();
             Undo.RecordObject(monsterMove, "Change Mode");
             monsterMove.roamingMode = roamingMode;
-            monsterMove.RoamingModeChange();
+
 
         }
 
@@ -46,7 +45,24 @@ public class MnsterMoveInspector : Editor
                 monsterMove.moveRomaingAreaPosionZ = EditorGUILayout.Slider("정찰영역 Z좌표이동", monsterMove.moveRomaingAreaPosionZ, -100.0f, 100.0f);
                 break;
             case MonsterMove.RoamingMode.지점순회:
-                SerializedProperty(roamingPoints,"roamingPoints");
+
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("정찰 지점(로컬좌표), Y값은 Z축으로 계산");
+
+
+                if (GUILayout.Button("추가"))
+                {
+                    Array.Resize(ref monsterMove.roamingPoints, monsterMove.roamingPoints.Length + 1);
+                }
+
+                else if (GUILayout.Button("감소") && monsterMove.roamingPoints.Length != 0)
+                {
+                    Array.Resize(ref monsterMove.roamingPoints, monsterMove.roamingPoints.Length - 1);
+                }
+
+                EditorGUILayout.EndHorizontal();
+                SerializedProperty(roamingPoints, "roamingPoints");
                 break;
         }
 
@@ -65,11 +81,29 @@ public class MnsterMoveInspector : Editor
     {
         property = serializedObject.FindProperty(name);
         EditorGUI.BeginChangeCheck();
-        EditorGUILayout.PropertyField(property, true);
+        EditorGUILayout.PropertyField(property,new GUIContent("정찰 지점"), true);
         if(EditorGUI.EndChangeCheck())
         {
             serializedObject.ApplyModifiedProperties();
         }
     }
+    public void RoamingModeChangeTest()
+    {
 
+        if (monsterMove.roamingMode == MonsterMove.RoamingMode.지점순회)
+        {
+            monsterMove.roamingAreaWidth = 0f;
+            monsterMove.roamingAreaHeight = 0f;
+            monsterMove.moveRomaingAreaPosionX = 0f;
+            monsterMove.moveRomaingAreaPosionZ = 0f;
+        }
+        else if (monsterMove.roamingMode == MonsterMove.RoamingMode.영역내무작위이동)
+        {
+            if (monsterMove.roamingPoints.Length != 0)
+            {
+                Array.Resize(ref monsterMove.roamingPoints, 0);
+            }
+        }
+
+    }
 }
--- Assets/02.Scripts/Monster/MonsterMove.cs
+++ Assets/02.Scripts/Monster/MonsterMove.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.AI;
 using System;
 
 public class MonsterMove : MonoBehaviour
@@ -10,14 +11,19 @@ public class MonsterMove : MonoBehaviour
     {
         지점순회, 영역내무작위이동
     };
+    public enum MonsterState
+    {
+        Roaming, Trace, Attack, Die
+    };
 
     /****************몬스터이동로직**********************/
     //기본적인 움직임은 네비게이션메쉬 사용(장애물인식을 원할하게하기위함)
     //인스펙터에서 로밍(정찰) 영역을 지정하게하려면 어떻게해야하는가
     //최종적으로는 지정한 영역외를 벗어나진못하되 
     //지정 영역내에서는 무작위로 행동하도록 구현목적
-    Vector3[] roamingArea;
-    public Vector3[] roamingPoints = new Vector3[0];
+    [HideInInspector]
+    public Vector3[] roamingArea;
+    public Vector2[] roamingPoints = new Vector2[0];
     [HideInInspector]
     public float roamingAreaWidth;
     [HideInInspector]
@@ -28,24 +34,64 @@ public class MonsterMove : MonoBehaviour
     public float moveRomaingAreaPosionZ;
 
     bool gameStart = false;
-    //몬스터의 최초위치
     Vector3 roamingAreaPosition;
-    Vector3 firstPosition;
+
     public RoamingMode roamingMode;
+    MonsterState state;
+    NavMeshAgent myNavMesh;
+    Transform target;
+    
     void Awake()
     {
-        //최초위치
-        firstPosition = transform.position;
+        roamingAreaPosition = new Vector3(transform.position.x + moveRomaingAreaPosionX, 0f, transform.position.z + moveRomaingAreaPosionZ);
+        myNavMesh = GetComponent<NavMeshAgent>();
+        MonsterState state = MonsterState.Roaming;
+        roamingArea = new Vector3[]
+        {
+            //왼쪽아래
+            new Vector3(roamingAreaPosition.x - roamingAreaWidth, 0, roamingAreaPosition.z - roamingAreaHeight),
+            //왼쪽 위
+            new Vector3(roamingAreaPosition.x - roamingAreaWidth, 0, roamingAreaPosition.z + roamingAreaHeight),
+            //오른쪽 위
+            new Vector3(roamingAreaPosition.x + roamingAreaWidth, 0, roamingAreaPosition.z + roamingAreaHeight),
+            //오른쪽 아래
+            new Vector3(roamingAreaPosition.x + roamingAreaWidth, 0, roamingAreaPosition.z - roamingAreaHeight)
+        };
         gameStart = true;
 
 
     }
-
     private void Update()
     {
-        roamingAreaPosition = new Vector3(firstPosition.x + moveRomaingAreaPosionX, 0f, firstPosition.z + moveRomaingAreaPosionZ);
-        if(roamingMode == RoamingMode.영역내무작위이동)
+        switch(state)
         {
+            case MonsterState.Roaming:
+                switch(roamingMode)
+                {
+                    case RoamingMode.지점순회:
+                        if(target !=null && roamingPoints.Length !=0)
+                        {
+                            myNavMesh.destination = roamingPoints[0];
+                        }
+
+                        break;
+                    case RoamingMode.영역내무작위이동:
+                        break;
+                }
+
+
+                break;
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        /******************정찰범위 디버그용**********************/
+        if (roamingMode == RoamingMode.영역내무작위이동 && !gameStart)
+        {
+            roamingAreaPosition = new Vector3(transform.position.x + moveRomaingAreaPosionX, 0f, transform.position.z + moveRomaingAreaPosionZ);
+
             roamingArea = new Vector3[]
             {
             //왼쪽아래
@@ -57,35 +103,21 @@ public class MonsterMove : MonoBehaviour
             //오른쪽 아래
             new Vector3(roamingAreaPosition.x + roamingAreaWidth, 0, roamingAreaPosition.z - roamingAreaHeight)
             };
+
+            Handles.DrawSolidRectangleWithOutline(roamingArea, new Color(1f, 1f, 1f, 0.2f), Color.white);
         }
 
-    }
-    private void OnDrawGizmos()
-    {
-        if(gameStart)
-            Handles.DrawSolidRectangleWithOutline(roamingArea, new Color(1f,1f,1f,0.2f),Color.white);
-    }
-
-    public void RoamingModeChange()
-    {
-
-        if (roamingMode == RoamingMode.지점순회)
+        else if (roamingMode == RoamingMode.지점순회 && roamingPoints.Length != 0 && !gameStart)
         {
-            Debug.Log(11);
-            roamingAreaWidth = 0f;
-            roamingAreaHeight = 0f;
-            moveRomaingAreaPosionX = 0f;
-            moveRomaingAreaPosionZ = 0f;
-        }
-        else if(roamingMode == RoamingMode.영역내무작위이동)
-        {
-            Debug.Log(22);
-            if(roamingPoints.Length !=0)
+            for (int i=0; i< roamingPoints.Length; ++i)
             {
-                Array.Resize(ref roamingPoints, 0);
+                Handles.Label(new Vector3(transform.position.x + roamingPoints[i].x, 1.5f, transform.position.z + roamingPoints[i].y), (i+1).ToString());
+                Gizmos.color = new Color(0f, 0f, 1f, 0.5f);
+                Gizmos.DrawSphere(new Vector3(transform.position.x + roamingPoints[i].x, 0, transform.position.z + roamingPoints[i].y), 1f);
+
+
             }
         }
-
     }
 
 }
