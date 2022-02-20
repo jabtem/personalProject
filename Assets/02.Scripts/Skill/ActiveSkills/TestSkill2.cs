268909f9f860b365aad972282adbdfb0d321ea78
jabtem <suchie777@gmail.com>
2022-02-21 04:07:16 +0900

    스킬 투사체처럼 발사할수있또록 수정 스킬 사용후 일정시간내에 충돌이없다면 이펙트매니저에 오브젝트풀

--- Assets/02.Scripts/Skill/ActiveSkills/TestSkill2.cs
+++ Assets/02.Scripts/Skill/ActiveSkills/TestSkill2.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class TestSkill2 : SkillBase
 {
 
+    float time;
+
     public override void SkillEffect()
     {
-        transform.Translate(Dir * Time.deltaTime);
+        transform.Translate(Dir * Time.deltaTime*2);
     }
 
-    private void Start()
+    private void OnEnable()
     {
         transform.position = new Vector3(Pos.x, 1.5f, Pos.z) + Dir * 3f;
     }
 
     private void Update()
     {
-
+        time += Time.deltaTime;
         SkillEffect();
+
+        if(time >= 3.0f)
+        {
+            time = 0f;
+            GameManager.instance.Effect.PushEffect(SkillNum, this.gameObject);
+        }
     }
 }
